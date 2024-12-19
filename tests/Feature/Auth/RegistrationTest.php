28d37fd6a30636c0028ee9c3a585abd912da756e
yukaty <254470+yukaty@users.noreply.github.com>
2024-12-19 14:27:53 -0700

    feat: Add email verification

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -31,6 +31,6 @@ class RegistrationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        $response->assertRedirect('/verify-email');
     }
 }
