32d5e8a6dacedb9d55135a9b38f03db82c6503ad
yukaty <254470+yukaty@users.noreply.github.com>
2024-12-19 13:58:43 -0700

    feat: Add user authentication

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,14 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'kana' => fake()->kanaName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'postal_code' => fake()->postcode(),
+            'address' => fake()->address(),
+            'phone_number' => fake()->phoneNumber(),
         ];
     }
 
@@ -37,7 +41,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
