d04eb9523a730ba012515e0b6af6746e0a7fb8ed
yukaty <254470+yukaty@users.noreply.github.com>
2024-12-20 19:07:29 -0700

    feat: Add admin authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,7 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
+    Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
+});
